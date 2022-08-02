ec16ca95fe193647bae4944c06b92b47e8d7f2ad
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-08-01 23:12:48 -0400

    refactor - item menu early returns null if no children or permissions

--- src/components/Shared/ItemMenu.tsx
+++ src/components/Shared/ItemMenu.tsx
@@ -34,72 +34,67 @@ const ItemMenu = ({
 }: Props) => {
   const t = useTranslate();
 
-  const handleMenuButtonClick = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
+  const handleMenuButtonClick = (event: React.MouseEvent<HTMLButtonElement>) =>
     setAnchorEl(event.currentTarget);
-  };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const handleClose = () => setAnchorEl(null);
 
-  if (canEdit || canDelete || isRenderable(children)) {
-    return (
-      <>
-        <IconButton
-          aria-label={t("labels.menuButton")}
-          onClick={handleMenuButtonClick}
-        >
-          <MoreHoriz sx={{ color: BLACK }} />
-        </IconButton>
-
-        <Menu
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          {prependChildren && children}
-
-          {canEdit && (
-            <MenuItem>
-              <Link
-                href={`/${itemType}s/${name ? name : itemId}${
-                  NavigationPaths.Edit
-                }`}
-              >
-                <Edit
-                  fontSize="small"
-                  sx={{
-                    marginRight: 1,
-                    transform: "rotateY(180deg) translateY(2px)",
-                  }}
-                />
-                {t("actions.edit")}
-              </Link>
-            </MenuItem>
-          )}
-
-          {canDelete && deleteItem && (
-            <MenuItem
-              onClick={() =>
-                window.confirm(t("prompts.deleteItem", { itemType })) &&
-                deleteItem(itemId)
-              }
-            >
-              <Delete fontSize="small" sx={{ marginRight: 1 }} />
-              {t("actions.delete")}
-            </MenuItem>
-          )}
-
-          {!prependChildren && children}
-        </Menu>
-      </>
-    );
+  if (!canEdit && !canDelete && !isRenderable(children)) {
+    return null;
   }
 
-  return null;
+  return (
+    <>
+      <IconButton
+        aria-label={t("labels.menuButton")}
+        onClick={handleMenuButtonClick}
+      >
+        <MoreHoriz sx={{ color: BLACK }} />
+      </IconButton>
+
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {prependChildren && children}
+
+        {canEdit && (
+          <MenuItem>
+            <Link
+              href={`/${itemType}s/${name ? name : itemId}${
+                NavigationPaths.Edit
+              }`}
+            >
+              <Edit
+                fontSize="small"
+                sx={{
+                  marginRight: 1,
+                  transform: "rotateY(180deg) translateY(2px)",
+                }}
+              />
+              {t("actions.edit")}
+            </Link>
+          </MenuItem>
+        )}
+
+        {canDelete && deleteItem && (
+          <MenuItem
+            onClick={() =>
+              window.confirm(t("prompts.deleteItem", { itemType })) &&
+              deleteItem(itemId)
+            }
+          >
+            <Delete fontSize="small" sx={{ marginRight: 1 }} />
+            {t("actions.delete")}
+          </MenuItem>
+        )}
+
+        {!prependChildren && children}
+      </Menu>
+    </>
+  );
 };
 
 export default ItemMenu;

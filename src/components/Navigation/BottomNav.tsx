cb0e16f629d435beee12d2c2fdda43e1885601fe
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-08-02 22:43:15 -0400

    feat - added i18n messages for all bottom nav labels

--- src/components/Navigation/BottomNav.tsx
+++ src/components/Navigation/BottomNav.tsx
@@ -5,6 +5,7 @@ import Router, { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { isNavDrawerOpenVar } from "../../client/cache";
 import { NavigationPaths, ResourceNames } from "../../constants/common";
+import { useTranslate } from "../../hooks/common";
 import { scrollTop } from "../../utils/common";
 import Link from "../Shared/Link";
 
@@ -22,7 +23,9 @@ const NavLink = ({ href, icon }: NavLinkProps) => (
 const BottomNav = () => {
   const [value, setValue] = useState(0);
   const isNavDrawerOpen = useReactiveVar(isNavDrawerOpenVar);
+
   const { asPath: currentPath } = useRouter();
+  const t = useTranslate();
 
   useEffect(() => {
     if (!isNavDrawerOpen) {
@@ -63,41 +66,41 @@ const BottomNav = () => {
   return (
     <Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 1 }}>
       <BottomNavigation
-        value={value}
         onChange={(_event, newValue) => {
           setValue(newValue);
         }}
         showLabels
+        value={value}
       >
         <BottomNavigationAction
-          onClick={() => handleHomeButtonClick()}
           icon={<NavLink href={NavigationPaths.Home} icon={<Home />} />}
-          label={"Home"}
+          label={t("navigation.home")}
+          onClick={() => handleHomeButtonClick()}
         />
 
         <BottomNavigationAction
-          onClick={() => Router.push(NavigationPaths.Events)}
+          disabled
           icon={
             <NavLink
               href={NavigationPaths.Events}
               icon={<EventNote style={{ marginBottom: -1 }} />}
             />
           }
-          label={"Events"}
-          disabled
+          label={t("navigation.events")}
+          onClick={() => Router.push(NavigationPaths.Events)}
         />
 
         <BottomNavigationAction
-          onClick={() => Router.push(NavigationPaths.Groups)}
+          disabled
           icon={<NavLink href={NavigationPaths.Groups} icon={<Group />} />}
-          label={"Groups"}
-          disabled
+          label={t("navigation.groups")}
+          onClick={() => Router.push(NavigationPaths.Groups)}
         />
 
         <BottomNavigationAction
-          onClick={() => isNavDrawerOpenVar(true)}
-          label={"Menu"}
           icon={<Menu />}
+          label={t("navigation.menu")}
+          onClick={() => isNavDrawerOpenVar(!isNavDrawerOpen)}
         />
       </BottomNavigation>
     </Paper>

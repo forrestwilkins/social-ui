7b10f3f6cbd899f14418711555934b840d20c42c
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-08-13 17:27:35 -0400

    feat - links in left nav pane now show as bold when active

--- src/components/Navigation/LeftNav.tsx
+++ src/components/Navigation/LeftNav.tsx
@@ -5,74 +5,124 @@ import {
   SupervisedUserCircle as UsersIcon,
 } from "@mui/icons-material";
 import {
-  Box,
   List,
   ListItemButton as MuiListItemButton,
-  ListItemIcon,
+  ListItemIcon as MuiListItemIcon,
   ListItemText as MuiListItemText,
+  ListItemTextProps as MuiListItemTextProps,
 } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { styled, SxProps } from "@mui/material/styles";
+import { useRouter } from "next/router";
 import { NavigationPaths } from "../../constants/common";
 import { useTranslate } from "../../hooks/common";
 import Link from "../Shared/Link";
+
+interface ListItemTextProps extends MuiListItemTextProps {
+  isActive?: boolean;
+}
+
+const ListItemText = styled(MuiListItemText, {
+  shouldForwardProp: (prop) => prop !== "isActive",
+})<ListItemTextProps>(({ isActive }) => ({
+  "& .MuiListItemText-primary": {
+    fontSize: 20,
+    ...(isActive && {
+      fontFamily: "Inter Bold",
+    }),
+  },
+}));
 
 const ListItemButton = styled(MuiListItemButton)(({ theme }) => ({
   borderRadius: 9999,
   color: theme.palette.text.primary,
 }));
 
-const ListItemText = styled(MuiListItemText)(() => ({
-  "& .MuiListItemText-primary": {
-    fontSize: 20,
-  },
+const ListItemIcon = styled(MuiListItemIcon)(() => ({
+  justifyContent: "center",
+  marginRight: 10,
+  minWidth: 40,
 }));
 
 const LeftNav = () => {
+  const { asPath } = useRouter();
   const t = useTranslate();
 
+  const listStyles: SxProps = {
+    position: "fixed",
+    left: 100,
+    top: 110,
+    width: 160,
+  };
+
+  const getIconStyle = (path: NavigationPaths) => {
+    const transition = { transition: "0.2s ease" };
+    if (asPath === path) {
+      return { fontSize: 28, ...transition };
+    }
+    return transition;
+  };
+
+  const isActive = (path: NavigationPaths) => path === asPath;
+
   return (
-    <Box
-      role="navigation"
-      sx={{ position: "fixed", top: 110, left: 108, width: 160 }}
-    >
-      <List component={"div"}>
-        <Link href={NavigationPaths.Home}>
-          <ListItemButton>
-            <ListItemIcon>
-              <HomeIcon color="primary" />
-            </ListItemIcon>
-            <ListItemText primary={t("navigation.home")} />
-          </ListItemButton>
-        </Link>
+    <List component={"div"} role="navigation" sx={listStyles}>
+      <Link href={NavigationPaths.Home}>
+        <ListItemButton>
+          <ListItemIcon>
+            <HomeIcon color="primary" sx={getIconStyle(NavigationPaths.Home)} />
+          </ListItemIcon>
+          <ListItemText
+            isActive={isActive(NavigationPaths.Home)}
+            primary={t("navigation.home")}
+          />
+        </ListItemButton>
+      </Link>
 
-        <Link href={NavigationPaths.Groups}>
-          <ListItemButton>
-            <ListItemIcon>
-              <GroupsIcon color="primary" />
-            </ListItemIcon>
-            <ListItemText primary={t("navigation.groups")} />
-          </ListItemButton>
-        </Link>
+      <Link href={NavigationPaths.Groups}>
+        <ListItemButton>
+          <ListItemIcon>
+            <GroupsIcon
+              color="primary"
+              sx={getIconStyle(NavigationPaths.Groups)}
+            />
+          </ListItemIcon>
+          <ListItemText
+            isActive={isActive(NavigationPaths.Groups)}
+            primary={t("navigation.groups")}
+          />
+        </ListItemButton>
+      </Link>
 
-        <Link href={NavigationPaths.Events}>
-          <ListItemButton>
-            <ListItemIcon>
-              <EventsIcon color="primary" />
-            </ListItemIcon>
-            <ListItemText primary={t("navigation.events")} />
-          </ListItemButton>
-        </Link>
+      <Link href={NavigationPaths.Events}>
+        <ListItemButton>
+          <ListItemIcon>
+            <EventsIcon
+              color="primary"
+              sx={getIconStyle(NavigationPaths.Events)}
+            />
+          </ListItemIcon>
+          <ListItemText
+            isActive={isActive(NavigationPaths.Events)}
+            primary={t("navigation.events")}
+          />
+        </ListItemButton>
+      </Link>
 
-        <Link href={NavigationPaths.Users}>
-          <ListItemButton>
-            <ListItemIcon>
-              <UsersIcon color="primary" />
-            </ListItemIcon>
-            <ListItemText primary={t("navigation.users")} />
-          </ListItemButton>
-        </Link>
-      </List>
-    </Box>
+      <Link href={NavigationPaths.Users}>
+        <ListItemButton>
+          <ListItemIcon>
+            <UsersIcon
+              color="primary"
+              sx={getIconStyle(NavigationPaths.Users)}
+            />
+          </ListItemIcon>
+          <ListItemText
+            isActive={isActive(NavigationPaths.Users)}
+            primary={t("navigation.users")}
+          />
+        </ListItemButton>
+      </Link>
+    </List>
   );
 };
 

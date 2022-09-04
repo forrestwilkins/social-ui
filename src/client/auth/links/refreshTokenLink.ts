ea7e4f924beb616ab960f0d918b6bd44d2f420d2
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-09-04 15:10:49 -0400

    fix: refresh token logic can now handle multiple failed requests

---
+++ src/client/auth/links/refreshTokenLink.ts
@@ -0,0 +1,97 @@
+import { Observable } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import { GraphQLError } from "graphql";
+import client from "../..";
+import { AuthResult } from "../../../types/auth";
+import { logOutUser } from "../../../utils/auth";
+import { isRefreshingTokenVar } from "../../cache";
+import { REFRESH_TOKEN_MUTATION } from "../mutations";
+
+type Callback = (arg: unknown) => void;
+
+let tokenSubscribers: Callback[] = [];
+
+const subscribeTokenRefresh = (cb: Callback) => {
+  tokenSubscribers.push(cb);
+};
+const onTokenRefreshed = (err: unknown) => {
+  tokenSubscribers.map((cb: Callback) => cb(err));
+};
+
+const refreshTokenLink = onError(
+  ({ graphQLErrors, networkError, operation, response, forward }) =>
+    new Observable((observer) => {
+      if (graphQLErrors) {
+        for (const { message, locations, path } of graphQLErrors) {
+          console.error(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          );
+        }
+
+        graphQLErrors.map(async ({ extensions }, index) => {
+          switch (extensions.code) {
+            case "UNAUTHENTICATED": {
+              const retryRequest = () => {
+                const subscriber = {
+                  complete: observer.complete.bind(observer),
+                  error: observer.error.bind(observer),
+                  next: observer.next.bind(observer),
+                };
+                return forward(operation).subscribe(subscriber);
+              };
+
+              if (!isRefreshingTokenVar()) {
+                try {
+                  const refreshed = await refreshToken();
+
+                  if (!refreshed) {
+                    throw new GraphQLError("Failed to refresh token");
+                  }
+
+                  onTokenRefreshed(null);
+                  tokenSubscribers = [];
+
+                  return retryRequest();
+                } catch (e) {
+                  return observer.error(graphQLErrors[index]);
+                }
+              }
+
+              const tokenSubscriber = new Promise((resolve) => {
+                subscribeTokenRefresh((errRefreshing: unknown) => {
+                  if (!errRefreshing) {
+                    return resolve(retryRequest());
+                  }
+                });
+              });
+
+              return tokenSubscriber;
+            }
+          }
+          return observer.next(response!);
+        });
+      }
+
+      if (networkError) {
+        console.error(`[Network error]: ${networkError}`);
+        return observer.error(networkError);
+      }
+    })
+);
+
+const refreshToken = async () => {
+  try {
+    isRefreshingTokenVar(true);
+    const { data } = await client.mutate<AuthResult>({
+      mutation: REFRESH_TOKEN_MUTATION,
+    });
+    return data?.refreshToken;
+  } catch (err) {
+    await logOutUser();
+    throw err;
+  } finally {
+    isRefreshingTokenVar(false);
+  }
+};
+
+export default refreshTokenLink;
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,98 +1,15 @@
-import {
-  ApolloClient,
-  createHttpLink,
-  FetchResult,
-  from,
-  Observable,
-} from "@apollo/client";
-import { onError } from "@apollo/client/link/error";
-import { GraphQLError } from "graphql";
+import { ApolloClient, createHttpLink, from } from "@apollo/client";
 import { API_ROOT } from "../constants/common";
-import { AuthResult } from "../types/auth";
-import { logOutUser } from "../utils/auth";
-import { REFRESH_TOKEN_MUTATION } from "./auth/mutations";
-import cache, { isRefreshingTokenVar } from "./cache";
+import refreshTokenLink from "./auth/links/refreshTokenLink";
+import cache from "./cache";
 
 const httpLink = createHttpLink({
   uri: `${API_ROOT}/graphql`,
 });
 
-const errorLink = onError(
-  ({ graphQLErrors, networkError, operation, forward }) => {
-    if (graphQLErrors) {
-      for (const { message, locations, path, extensions } of graphQLErrors) {
-        console.error(
-          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-        );
-
-        switch (extensions.code) {
-          case "UNAUTHENTICATED": {
-            // Ignores 401 errors for refresh requests
-            if (operation.operationName === "RefreshTokenMutation") {
-              return;
-            }
-
-            const observable = new Observable<FetchResult<Record<string, any>>>(
-              (observer) => {
-                (async () => {
-                  try {
-                    // Skips refresh logic if already refreshing
-                    if (!isRefreshingTokenVar()) {
-                      const refreshed = await refreshToken();
-                      if (!refreshed) {
-                        throw new GraphQLError("Failed to refresh token");
-                      }
-                    }
-
-                    // Retry the failed request
-                    const subscriber = {
-                      next: observer.next.bind(observer),
-                      error: observer.error.bind(observer),
-                      complete: observer.complete.bind(observer),
-                    };
-                    forward(operation).subscribe(subscriber);
-                  } catch (err) {
-                    observer.error(err);
-                  }
-                })();
-              }
-            );
-
-            return observable;
-          }
-        }
-      }
-    }
-    if (networkError) {
-      console.error(`[Network error]: ${networkError}`);
-    }
-  }
-);
-
 const client = new ApolloClient({
-  link: from([errorLink, httpLink]),
+  link: from([refreshTokenLink, httpLink]),
   cache,
 });
-
-/**
- * FIXME: Support for refresh tokens is currently broken. When attempting to make multiple
- * requests at once, with an expired access token, the users access token will successfully
- * be refreshed. However, at least one of the requests will fail to go through completely,
- * which is then causing the user to be signed out.
- */
-export const refreshToken = async () => {
-  try {
-    isRefreshingTokenVar(true);
-    const { data } = await client.mutate<AuthResult>({
-      mutation: REFRESH_TOKEN_MUTATION,
-    });
-    return data?.refreshToken;
-  } catch (err) {
-    await logOutUser();
-    throw err;
-  } finally {
-    isRefreshingTokenVar(false);
-  }
-};
 
 export default client;

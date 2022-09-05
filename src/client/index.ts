63ba57d9d02b8fec90bb77158fa13982de86b8e7
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-09-05 13:20:29 -0400

    feat: installed apollo-upload-client, added createUploadLink to client

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,14 +1,15 @@
-import { ApolloClient, createHttpLink, from } from "@apollo/client";
+import { ApolloClient, from } from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
 import { API_ROOT } from "../constants/common";
 import refreshTokenLink from "./auth/links/refreshTokenLink";
 import cache from "./cache";
 
-const httpLink = createHttpLink({
+const terminatingLink = createUploadLink({
   uri: `${API_ROOT}/graphql`,
 });
 
 const client = new ApolloClient({
-  link: from([refreshTokenLink, httpLink]),
+  link: from([refreshTokenLink, terminatingLink]),
   cache,
 });
 

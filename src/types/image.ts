040550cae5cd2048afcc4448bcdc32ec38cbb920
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-09-17 21:14:34 -0400

    refactor: converted optional types to required for ImageEntity type

--- src/types/image.ts
+++ src/types/image.ts
@@ -1,10 +1,10 @@
 export interface ImageEntity {
   id: number;
   filename: string;
-  imageType?: string;
-  postId?: number;
-  userId?: number;
-  createdAt?: string;
-  updatedAt?: string;
+  imageType: string;
+  postId: number;
+  userId: number;
+  createdAt: string;
+  updatedAt: string;
   __typename: string;
 }

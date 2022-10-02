a0dd13bb4f41a416a9797b60952f315e4e5c0da3
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-10-02 19:06:08 -0400

    feat: added gap between stacked images

--- src/components/Images/ImageList.tsx
+++ src/components/Images/ImageList.tsx
@@ -8,13 +8,14 @@ interface Props extends Omit<BoxProps, "children"> {
 
 const ImageList = ({ images, sx, ...boxProps }: Props) => (
   <Box sx={{ marginX: -2, ...sx }} {...boxProps}>
-    {images.map((image) => (
+    {images.map((image, index) => (
       <img
         alt={image.filename}
         key={image.id}
         src={getImagePath(image.id)}
         style={{
           display: "block",
+          marginBottom: index + 1 === images.length ? undefined : 2,
           width: "100%",
         }}
       />

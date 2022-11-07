3ea6b70eee647cc59d83d12e0e095bc1a1125564
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-11-07 18:58:14 -0500

    refactor: removed dupe User and Post types

--- src/types/post.types.ts
+++ src/types/post.types.ts
@@ -1,16 +1,4 @@
-import { Group, Image } from "./generated.types";
-import { User } from "./user.types";
-
-export interface Post {
-  id: number;
-  body: string;
-  images: Image[];
-  group: Group | null;
-  user: User;
-  createdAt: string;
-  updatedAt: string;
-  __typename: string;
-}
+import { Post } from "./generated.types";
 
 export interface PostsFormValues extends Pick<Post, "body"> {
   groupId?: number;
--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,19 +1,3 @@
-import { Image } from "./generated.types";
-import { Post } from "./post.types";
-
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  bio: string;
-  profilePicture: Image;
-  coverPhoto: Image | null;
-  posts: Post[];
-  createdAt: string;
-  updatedAt: string;
-  __typename: string;
-}
-
 export interface UserFormValues {
   password?: string;
   name?: string;

f2d45d9f18be6f2001874458fecc5708c427e2ed
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-11-12 18:39:50 -0500

    refactor: replaced updateFragment call with cache.modify for updating group after deletePost mutation

--- src/apollo/posts/mutations/delete-post.mutation.ts
+++ src/apollo/posts/mutations/delete-post.mutation.ts
@@ -1,12 +1,10 @@
-import { ApolloCache, gql } from "@apollo/client";
+import { ApolloCache, gql, Reference } from "@apollo/client";
 import produce from "immer";
 import { TypeNames } from "../../../constants/common.constants";
 import {
-  GroupProfileFragment,
   PostsQuery,
   UserProfileFragment,
 } from "../../../types/generated.types";
-import GROUP_PROFILE_FRAGMENT from "../../groups/fragments/group-profile.fragment";
 import USER_PROFILE_FRAGMENT from "../../users/fragments/user-profile.fragment";
 import POSTS_QUERY from "../queries/posts.query";
 
@@ -46,21 +44,14 @@ export const removePost =
     if (!groupId) {
       return;
     }
-    cache.updateFragment<GroupProfileFragment>(
-      {
-        id: cache.identify({ id: groupId, __typename: TypeNames.Group }),
-        fragment: GROUP_PROFILE_FRAGMENT,
-        fragmentName: "GroupProfile",
+    cache.modify({
+      id: cache.identify({ id: groupId, __typename: TypeNames.Group }),
+      fields: {
+        posts(existingPostRefs: Reference[], { readField }) {
+          return existingPostRefs.filter((ref) => id !== readField("id", ref));
+        },
       },
-      (data) =>
-        produce(data, (draft) => {
-          if (!draft) {
-            return;
-          }
-          const index = draft.posts.findIndex((p) => p.id === id);
-          draft.posts.splice(index, 1);
-        })
-    );
+    });
   };
 
 export default DELETE_POST_MUTATION;

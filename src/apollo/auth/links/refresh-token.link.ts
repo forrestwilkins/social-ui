83cfb68e8960975a36367240c19005f19fe1cc75
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-12-03 13:48:23 -0500

    fix: refreshTokenLink works with graphql-shield error message

--- src/apollo/auth/links/refresh-token.link.ts
+++ src/apollo/auth/links/refresh-token.link.ts
@@ -1,6 +1,9 @@
 import { Observable } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
-import { MutationNames } from "../../../constants/common.constants";
+import {
+  MutationNames,
+  UNAUTHORIZED,
+} from "../../../constants/common.constants";
 import { isRefreshingTokenVar } from "../../cache";
 import { refreshToken } from "../mutations/RefreshToken.mutation";
 
@@ -23,56 +26,51 @@ const refreshTokenLink = onError(
   ({ graphQLErrors, networkError, operation, response, forward }) =>
     new Observable((observer) => {
       if (graphQLErrors) {
-        graphQLErrors.map(
-          async ({ extensions, message, locations, path }, index) => {
-            console.error(
-              `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-            );
-            if (!response) {
+        graphQLErrors.map(async ({ message, locations, path }, index) => {
+          console.error(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          );
+          if (!response) {
+            return observer.error(graphQLErrors[index]);
+          }
+
+          if (message === UNAUTHORIZED) {
+            // Ignores 401 errors for refresh requests
+            if (operation.operationName === MutationNames.RefreshToken) {
               return observer.error(graphQLErrors[index]);
             }
 
-            switch (extensions.code) {
-              case "UNAUTHENTICATED": {
-                // Ignores 401 errors for refresh requests
-                if (operation.operationName === MutationNames.RefreshToken) {
-                  return observer.error(graphQLErrors[index]);
-                }
+            const retryRequest = () => {
+              const subscriber = {
+                complete: observer.complete.bind(observer),
+                error: observer.error.bind(observer),
+                next: observer.next.bind(observer),
+              };
+              return forward(operation).subscribe(subscriber);
+            };
 
-                const retryRequest = () => {
-                  const subscriber = {
-                    complete: observer.complete.bind(observer),
-                    error: observer.error.bind(observer),
-                    next: observer.next.bind(observer),
-                  };
-                  return forward(operation).subscribe(subscriber);
-                };
-
-                if (!isRefreshingTokenVar()) {
-                  try {
-                    await refreshToken();
-                    prepareExit();
-                    return retryRequest();
-                  } catch {
-                    prepareExit();
-                    return observer.error(graphQLErrors[index]);
-                  }
-                }
-
-                const tokenSubscriber = new Promise((resolve) => {
-                  subscribeTokenRefresh((errRefreshing: unknown) => {
-                    if (!errRefreshing) {
-                      return resolve(retryRequest());
-                    }
-                  });
-                });
-
-                return tokenSubscriber;
+            if (!isRefreshingTokenVar()) {
+              try {
+                await refreshToken();
+                prepareExit();
+                return retryRequest();
+              } catch {
+                prepareExit();
+                return observer.error(graphQLErrors[index]);
               }
             }
-            return observer.next(response);
+
+            const tokenSubscriber = new Promise((resolve) => {
+              subscribeTokenRefresh((errRefreshing: unknown) => {
+                if (!errRefreshing) {
+                  return resolve(retryRequest());
+                }
+              });
+            });
+            return tokenSubscriber;
           }
-        );
+          return observer.next(response);
+        });
       }
 
       if (networkError) {

2f0b5d58b1438be17b95f4b6818bae3fd36d3c4c
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-12-11 20:41:27 -0500

    feat: add breadcrumbs for edit roles page

--- src/pages/roles/[id]/edit.tsx
+++ src/pages/roles/[id]/edit.tsx
@@ -3,8 +3,11 @@
 import { Card, CardContent, Typography } from "@mui/material";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { breadcrumbsVar } from "../../../apollo/cache";
 import { useEditRoleQuery } from "../../../apollo/gen";
 import ProgressBar from "../../../components/Shared/ProgressBar";
+import { NavigationPaths } from "../../../constants/common.constants";
 import { useTranslate } from "../../../hooks/common.hooks";
 
 const EditRole: NextPage = () => {
@@ -14,8 +17,27 @@ const EditRole: NextPage = () => {
     variables: { id },
     skip: !id,
   });
+  const role = data?.role;
 
+  const { asPath } = useRouter();
   const t = useTranslate();
+
+  useEffect(() => {
+    if (role) {
+      breadcrumbsVar({
+        path: asPath,
+        breadcrumbs: [
+          {
+            label: t("roles.headers.serverRoles"),
+            href: NavigationPaths.Roles,
+          },
+          {
+            label: role.name,
+          },
+        ],
+      });
+    }
+  }, [t, asPath, role]);
 
   if (error) {
     return <Typography>{t("errors.somethingWentWrong")}</Typography>;

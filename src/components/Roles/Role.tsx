c4f80e05111331c478011d9b631420ae149479f6
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-12-13 21:07:21 -0500

    feat: role component links to edit role page without showing link text

--- src/components/Roles/Role.tsx
+++ src/components/Roles/Role.tsx
@@ -3,26 +3,20 @@
 import { Typography } from "@mui/material";
 import { RoleFragment } from "../../apollo/gen";
 import { NavigationPaths } from "../../constants/common.constants";
-import { useTranslate } from "../../hooks/common.hooks";
-import Flex from "../Shared/Flex";
 import Link from "../Shared/Link";
 
 interface Props {
   role: RoleFragment;
-  hideEdit?: boolean;
 }
 
-const Role = ({ role: { id, name, color }, hideEdit }: Props) => {
-  const t = useTranslate();
+const Role = ({ role: { id, name, color } }: Props) => {
   const editRolePath = `${NavigationPaths.Roles}/${id}/edit`;
-
   return (
-    <Flex>
+    <Link href={editRolePath}>
       <Typography marginRight={1} color={color}>
         {name}
       </Typography>
-      {!hideEdit && <Link href={editRolePath}>{t("actions.edit")}</Link>}
-    </Flex>
+    </Link>
   );
 };
 

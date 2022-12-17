ff070efd05cbf863fdf465de99414ea71ae39b28
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-12-17 18:44:19 -0500

    feat: add wip tabs to edit role page for permissions and members

--- src/pages/roles/[id]/edit.tsx
+++ src/pages/roles/[id]/edit.tsx
@@ -1,18 +1,26 @@
 // TODO: Add remaining layout and functionality - below is a WIP
 
-import { Typography } from "@mui/material";
+import { Card, styled, Tab as MuiTab, Tabs, Typography } from "@mui/material";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { SyntheticEvent, useEffect, useState } from "react";
 import { breadcrumbsVar } from "../../../apollo/cache";
 import { useEditRoleQuery } from "../../../apollo/gen";
 import DeleteRoleButton from "../../../components/Roles/DeleteRoleButton";
 import RoleForm from "../../../components/Roles/RoleForm";
 import ProgressBar from "../../../components/Shared/ProgressBar";
 import { NavigationPaths } from "../../../constants/common.constants";
-import { useTranslate } from "../../../hooks/common.hooks";
+import { useAboveBreakpoint, useTranslate } from "../../../hooks/common.hooks";
+
+const Tab = styled(MuiTab)(({ theme }) => ({
+  [theme.breakpoints.up("sm")]: {
+    minWidth: 160,
+  },
+}));
 
 const EditRole: NextPage = () => {
+  const [tab, setTab] = useState(0);
+
   const { query } = useRouter();
   const id = parseInt(String(query?.id));
   const { data, loading, error } = useEditRoleQuery({
@@ -21,6 +29,7 @@ const EditRole: NextPage = () => {
   });
 
   const { asPath } = useRouter();
+  const isAboveSmall = useAboveBreakpoint("sm");
   const t = useTranslate();
 
   useEffect(() => {
@@ -40,6 +49,9 @@ const EditRole: NextPage = () => {
     }
   }, [t, asPath, data]);
 
+  const handleTabChange = (_: SyntheticEvent<Element, Event>, value: number) =>
+    setTab(value);
+
   if (error) {
     return <Typography>{t("errors.somethingWentWrong")}</Typography>;
   }
@@ -54,9 +66,27 @@ const EditRole: NextPage = () => {
 
   return (
     <>
-      <RoleForm editRole={data.role} />
+      <Card sx={{ marginBottom: 6 }}>
+        <Tabs
+          onChange={handleTabChange}
+          value={tab}
+          variant={isAboveSmall ? "standard" : "fullWidth"}
+          centered
+        >
+          <Tab label={t("roles.tabs.display")} />
+          <Tab label={t("roles.tabs.permissions")} />
+          <Tab label={t("roles.tabs.members")} />
+        </Tabs>
+      </Card>
 
-      <DeleteRoleButton roleId={data.role.id} />
+      {tab === 0 && (
+        <>
+          <RoleForm editRole={data.role} />
+          <DeleteRoleButton roleId={data.role.id} />
+        </>
+      )}
+
+      {tab !== 0 && <>{t("prompts.featureInDevelopment")}</>}
     </>
   );
 };

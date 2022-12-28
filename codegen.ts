8cae20772fa51e6c2c591fa0d2146628d4d8dcd1
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-12-28 17:55:07 -0500

    chore: update codegen config to only check .graphql files

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const config: CodegenConfig = {
   schema: `${process.env.API_URL}/graphql`,
-  documents: ["src/**/*.{tsx,ts,graphql}"],
+  documents: ["src/apollo/**/*.graphql"],
   ignoreNoDocuments: true,
 
   generates: {

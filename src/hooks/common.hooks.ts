4f6ab351e480319adb2896647813481a712f348b
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-12-29 21:31:05 -0500

    refactor: remove unused custom hooks

--- src/hooks/common.hooks.ts
+++ src/hooks/common.hooks.ts
@@ -1,18 +1,11 @@
 import { Breakpoint, useMediaQuery, useTheme } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
 import { BrowserEvents } from "../constants/common.constants";
 
 export const useAboveBreakpoint = (breakpoint: Breakpoint) =>
   useMediaQuery(useTheme().breakpoints.up(breakpoint));
 
 export const useIsDesktop = () => useAboveBreakpoint("md");
-
-// TODO: Remove and replace all instances with useTranslation
-export const useTranslate = () => {
-  const { t } = useTranslation();
-  return t;
-};
 
 export const useScrollPosition = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -32,16 +25,4 @@ export const useScrollPosition = () => {
   }, []);
 
   return scrollPosition;
-};
-
-export const useOnEvent = (
-  event: BrowserEvents,
-  handleEvent: (e: any) => void
-) => {
-  useEffect(() => {
-    window.addEventListener(event, handleEvent);
-    return () => {
-      window.removeEventListener(event, handleEvent);
-    };
-  }, [event, handleEvent]);
 };

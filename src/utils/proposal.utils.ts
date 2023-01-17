ac535464ba8829952d9a3d8510b7e866a79c22a4
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-01-16 21:26:17 -0500

    feat: only show proposal action options implemented so far

--- src/utils/proposal.utils.ts
+++ src/utils/proposal.utils.ts
@@ -5,41 +5,43 @@ export const getProposalActionTypeOptions = (
   t: TFunction<Namespace<"ns1">, undefined>
 ) => [
   {
-    message: t("proposals.actionTypes.assignRole"),
-    value: ProposalActionTypes.AssignRole,
-  },
-  {
-    message: t("proposals.actionTypes.changeCoverPhoto"),
-    value: ProposalActionTypes.ChangeCoverPhoto,
+    message: t("proposals.actionTypes.changeName"),
+    value: ProposalActionTypes.ChangeName,
   },
   {
     message: t("proposals.actionTypes.changeDescription"),
     value: ProposalActionTypes.ChangeDescription,
   },
   {
-    message: t("proposals.actionTypes.changeName"),
-    value: ProposalActionTypes.ChangeName,
-  },
-  {
-    message: t("proposals.actionTypes.changeRole"),
-    value: ProposalActionTypes.ChangeRole,
-  },
-  {
-    message: t("proposals.actionTypes.changeSettings"),
-    value: ProposalActionTypes.ChangeSettings,
-  },
-  {
-    message: t("proposals.actionTypes.createRole"),
-    value: ProposalActionTypes.CreateRole,
-  },
-  {
-    message: t("proposals.actionTypes.planEvent"),
-    value: ProposalActionTypes.PlanEvent,
-  },
-  {
     message: t("proposals.actionTypes.test"),
     value: ProposalActionTypes.Test,
   },
+
+  // TODO: Uncomment after adding support for remaining action types
+  // {
+  //   message: t("proposals.actionTypes.assignRole"),
+  //   value: ProposalActionTypes.AssignRole,
+  // },
+  // {
+  //   message: t("proposals.actionTypes.changeCoverPhoto"),
+  //   value: ProposalActionTypes.ChangeCoverPhoto,
+  // },
+  // {
+  //   message: t("proposals.actionTypes.changeRole"),
+  //   value: ProposalActionTypes.ChangeRole,
+  // },
+  // {
+  //   message: t("proposals.actionTypes.changeSettings"),
+  //   value: ProposalActionTypes.ChangeSettings,
+  // },
+  // {
+  //   message: t("proposals.actionTypes.createRole"),
+  //   value: ProposalActionTypes.CreateRole,
+  // },
+  // {
+  //   message: t("proposals.actionTypes.planEvent"),
+  //   value: ProposalActionTypes.PlanEvent,
+  // },
 ];
 
 export const getProposalActionLabel = (

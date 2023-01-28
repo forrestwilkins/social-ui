8881a3617228f18589475a0ce727f6e091d29c2b
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-01-27 23:09:23 -0500

    feat: update wip message color for events page

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,17 +1,21 @@
 // TODO: Add basic functionality for events - below is a WIP
 
-import { Typography } from "@mui/material";
+import { Typography, useTheme } from "@mui/material";
 import { NextPage } from "next";
 import { useTranslation } from "react-i18next";
 import LevelOneHeading from "../../components/Shared/LevelOneHeading";
 
 const EventsIndex: NextPage = () => {
   const { t } = useTranslation();
+  const theme = useTheme();
 
   return (
     <>
       <LevelOneHeading header>{t("navigation.events")}</LevelOneHeading>
-      <Typography gutterBottom>{t("prompts.wip")}</Typography>
+
+      <Typography sx={{ color: theme.palette.text.secondary }} gutterBottom>
+        {t("prompts.wip")}
+      </Typography>
     </>
   );
 };

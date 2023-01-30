cec123e5cecdfd99b5f05b808064225b22dd88d2
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-01-29 19:25:30 -0500

    fix: add type assertion for terminatingLink in apollo client

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,12 +1,13 @@
-import { ApolloClient, from } from "@apollo/client";
+import { ApolloClient, ApolloLink, from } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
 import { API_ROOT, Environments } from "../constants/common.constants";
 import refreshTokenLink from "./auth/links/refresh-token.link";
 import cache from "./cache";
 
+// TODO: Refactor to avoid type assertion
 const terminatingLink = createUploadLink({
   uri: `${API_ROOT}/graphql`,
-});
+}) as unknown as ApolloLink;
 
 const client = new ApolloClient({
   link: from([refreshTokenLink, terminatingLink]),
